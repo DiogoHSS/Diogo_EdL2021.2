6048ee551db86202eae33781dd887d8138fcad12
DiogoHSS <diogohssorio@gmail.com>
2022-02-20 12:34:25 -0300

    print_python2

---
+++ print_python2.py
@@ -0,0 +1,13 @@
+# print(<expressão>, <expressão> + <expressão>)
+
+# a saída de todas as expressões abaixo é 'Ola mundo'
+print("Ola mundo")  # nao tem diferenca entre usar parenteses ou nao
+print "Ola mundo"
+print "Ola", "mundo"
+print "Ola" + " " + "mundo"
+# -------------------------------
+
+variavel = 20
+print 10 + 20  # saída: '30'
+print "10" + "20"  # saída: '1020'
+print 10 + variavel  # saída: '30'
