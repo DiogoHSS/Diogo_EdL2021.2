beb971b096d4a259d7569eeed6f47e3f82c64cec
DiogoHSS <diogohssorio@gmail.com>
2022-02-20 12:40:56 -0300

    print python

--- print_python2.py
+++ print_python2.py
@@ -4,10 +4,5 @@
 print("Ola mundo")  # nao tem diferenca entre usar parenteses ou nao
 print "Ola mundo"
 print "Ola", "mundo"
-print "Ola" + " " + "mundo"
+print "Ola" + ' ' + "mundo"
 # -------------------------------
-
-variavel = 20
-print 10 + 20  # saída: '30'
-print "10" + "20"  # saída: '1020'
-print 10 + variavel  # saída: '30'
---
+++ print_python3.py
@@ -0,0 +1,13 @@
+# print(*objects, sep=' ', end='\n', file=sys.stout, flush=False)
+
+# todas as saídas abaixo são 'Ola mundo'
+print("Ola mundo")
+print("Ola", "mundo")
+print("Ola" + ' ' + "mundo")
+print("Ola", ' ', "mundo", sep='')
+# -
+print("Ola", end='')
+print(' ', end='')
+print("mundo",)
+# -
+# ------------------------------------
